83cf95b1525cfe9993998df646447e9d535cc258
lala9663 <lala96632040@gmail.com>
2023-06-10 22:36:49 +0900

    entity 다시 설계중

--- src/main/java/com/example/lottojpa/member/exception/MemberException.java
+++
@@ -1,8 +0,0 @@
-package com.example.lottojpa.member.exception;
-
-public class MemberException extends RuntimeException{
-    public MemberException(String message) {
-        super(message);
-    }
-
-}
