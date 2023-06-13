6fa77354b52ec0c2e58830ecdf8c48589a432c1d
lala9663 <lala96632040@gmail.com>
2023-06-13 17:41:57 +0900

    회원 repo 기초 설정

---
+++ src/main/java/com/example/lotto/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package com.example.lotto.repository;
+
+import com.example.lotto.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findById(String id) {
+        return em.createQuery("select m from Member m where m.id =:id", Member.class)
+                .setParameter("id", id)
+                .getResultList();
+    }
+
+}
